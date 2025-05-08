adc37c5852e834a957377c4373ea296c3fae498b
Harsh35944 <snehadhameliya359@gmail.com>
2025-05-08 14:35:11 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
     setError("");
     try {
-      const res = await axios.post("https://duplica-pksl-3p2eaqg46-harshs-projects-0bc5ca80.vercel.app/llogin", { email, password });
+      const res = await axios.post("http://localhost:3001/auth/llogin", { email, password });
       if (res.data.access_token) {
         localStorage.setItem("token", res.data.access_token);
         navigate("/");
